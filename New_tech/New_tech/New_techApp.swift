6ae106f7b405ff710b391e211c687656c616d188
Georgii <hodik123@gmail.com>
2022-01-28 21:53:51 +0300

    Init commit

---
+++ New_tech/New_tech/New_techApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_techApp.swift
+//  New_tech
+//
+//  Created by Георгий Ходиков on 28.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct New_techApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
